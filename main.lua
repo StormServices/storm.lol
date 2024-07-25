9337f2ab2d0d27e499863ce79a06f4e589625f13
StormServices <officialstormservices@gmail.com>
2024-07-25 14:11:17 -0400

    update

--- main.lua
+++ main.lua
@@ -54,9 +54,17 @@ local Misc = Tab5:AddSection("Misc", 1) -- idk
 local PlrStuff = Tab5:AddSection("Player", 2) -- make tp/view/bring
 --------------------------------------------------------------------
 
--- Services
+-- // Services
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
+<<<<<<< HEAD
+local Lighting = game:GetService("Lighting")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- // Variables
+
+=======
+>>>>>>> a8275421e1a283730be080668751bce991702251
 local Camera = workspace.CurrentCamera
 
 -- Variables
@@ -64,6 +72,107 @@ local LocalPlayer = Players.LocalPlayer
 local NameSize = 14 -- Default size for Name ESP
 local WorldToViewportPoint = Camera.WorldToViewportPoint
 
+<<<<<<< HEAD
+local CurrentCamera = Camera
+local WorldToViewportPoint = CurrentCamera.WorldToViewportPoint
+--------------------------------------------------------------------
+
+Main:AddToggle({text = "Aimbot", state = false, risky = false, tooltip = "", flag = "Toggle_1", risky = false, callback = function(v)
+    if v then
+        -- Aimbot code
+        while true do
+            local closestPlayer = nil
+            local closestDistance = math.huge
+            for _, player in pairs(game.Players:GetPlayers()) do
+                if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild(targetPart) then
+                    local character = player.Character
+                    local targetPosition = character[targetPart].Position
+                    local distance = (targetPosition - camera.CFrame.Position).Magnitude
+                    if distance < closestDistance and distance < fovRadius then
+                        closestDistance = distance
+                        closestPlayer = player
+                    end
+                end
+            end
+            if closestPlayer then
+                local targetPosition = closestPlayer.Character[targetPart].Position
+                local cameraPosition = camera.CFrame.Position
+                local direction = (targetPosition - cameraPosition).Unit
+                camera.CFrame = camera.CFrame:Lerp(CFrame.new(cameraPosition + direction * smoothness), 0.1)
+            end
+            wait()
+        end
+    end
+end})
+Main:AddToggle({text = "Toggle", state = false, risky = false, tooltip = "", flag = "Toggle_1", risky = false, callback = function(v)
+    print(notMade)
+end})
+Main:AddList({enabled = true, text = "Aim Part", tooltip = "", selected = "Head", multi = false, open = false, max = 4, values = {"Head", "Neck", "Torso", "HumanoidRootPart"}, risky = false, callback = function(v)
+    targetPart = v
+end})
+Main:AddBind({enabled = true, text = "Lock Keybind", mode = "toggle", bind = "Mouse", flag = "ToggleKey_1", state = false, nomouse = false, noindicator = false, callback = function(v)
+    bind = v
+end})
+Main:AddSeparator({enabled = true, text = "Checkers"})
+Main:AddToggle({text = "Visible Check", state = true, risky = false, tooltip = "If enabled, assist will only work if not behind a wall/surface.", flag = "wallCheck", risky = false, callback = function(v)
+    print(notMade)
+end})
+Main:AddToggle({text = "Status Check", state = true, risky = false, tooltip = "Prevent from locking on dead players", flag = "aliveCheck", risky = false, callback = function(v)
+    print(notMade)
+end})
+Main:AddToggle({text = "Whitelist", state = false, risky = false, tooltip = "Whitelist people away from the aimbot.", flag = "friendCheck", risky = false, callback = function(v)
+    print(notMade)
+end})
+Main:AddList({enabled = true, text = "Whitelist",  tooltip = "Whitelist Here", selected = "", multi = false, open = false,max = 1, values = {"Friend list", "Whitelisted"}, risky = false, callback = function(v)
+    print(notMade)
+end})
+LPlayer:AddSlider({enabled = true, text = "Speed", tooltip = "", flag = "LegitplrSpeed", suffix = "", dragging = true, focused = false, min = 0, max = 60, value = 16,  increment = 0.1, risky = false, callback = function(v)
+    LegitspeedValue = v
+end})
+LPlayer:AddSlider({enabled = true, text = "Jump", tooltip = "", flag = "LegitplrJump", suffix = "", dragging = true, focused = false, min = 0, max = 500, value = 50, increment = 0.1, risky = false, callback = function(v)
+    LegitjumpValue = v
+end})
+LPlayer:AddToggle({enabled = true, text = "Enable Speed", state = false, risky = false, tooltip = "", flag = "", risky = false, callback = function(v)
+    if v == true then
+        local speed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+        local oldSpeed = speed
+        speed = LegitspeedValue
+    else
+        speed = oldSpeed
+    end
+end})
+LPlayer:AddToggle({enabled = true, text = "Enable Jump", state = false, risky = false, tooltip = "", flag = "", risky = false, callback = function(v)
+    if v then
+        local jump = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+        local oldJump = jump
+        jump = LegitjumpValue
+    else
+        jump = oldJump
+    end
+end})
+LPlayer:AddButton({enabled = true, text = "Reset", tooltip = "Reset if game doesn't have reset enabled", confirm = true, risky = true, callback = function()
+    if resetMethod == 'HP' then
+        local plrHealth = game.Players.LocalPlayer.Character.Humanoid.Health
+
+        local decreaseRate = plrHealth / (HPtiming / 0.1)
+
+        while plrHealth > 0 do
+            plrHealth = plrHealth - decreaseRate
+            wait(0.1)
+        end
+    elseif resetMethod == 'IHP' then
+        game.Players.LocalPlayer.Character.Humanoid.Health = 0
+    elseif resetMethod == 'TP' then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -999999999, 0)
+    end
+end})
+LPlayer:AddList({enabled = true, text = "Reset method", tooltip = "HP - Will decrease plr hp to 0 slowly\n IHP - Same as HP but instantly\n TP - Tp plr to void", selected = "HP", multi = false, open = true, max = 3, values = {"HP", "IHP", "TP"}, risky = false, callback = function(v)
+    resetMethod = v
+end})
+
+
+=======
+>>>>>>> a8275421e1a283730be080668751bce991702251
 --// Glow ESP
 local ESPSettings = {
     ChamsColor = Color3.fromRGB(200, 200, 200), -- Default Chams Color
@@ -180,6 +289,14 @@ local function Spinbot()
 end
 
 --// Toggle Features
+<<<<<<< HEAD
+ESP:AddToggle({text = "Enable Esp", state = false, risky = false, tooltip = "", flag = "NameESP", callback = function(v)
+    if EnableESP then
+end})
+ESP:AddToggle({text = "Name", state = false, risky = false, tooltip = "", flag = "NameESP", callback = function(v)
+    NameESP = v
+end})
+=======
 ESP:AddToggle({
     text = "Name Esp",
     state = false,
@@ -190,7 +307,13 @@ ESP:AddToggle({
         NameESP = v
     end
 })
+>>>>>>> a8275421e1a283730be080668751bce991702251
 
+<<<<<<< HEAD
+ESP:AddToggle({text = "Box", state = false, risky = false, tooltip = "", flag = "BoxESP", callback = function(v)
+    BoxESP = v
+end})
+=======
 -- Slider for Name Size
 ESP:AddSlider({
     text = "Name Size",
@@ -204,7 +327,13 @@ ESP:AddSlider({
         NameSize = v
     end
 })
+>>>>>>> a8275421e1a283730be080668751bce991702251
 
+<<<<<<< HEAD
+ESP:AddToggle({text = "Glow", state = false, risky = false, tooltip = "", flag = "GlowESP", callback = function(v)
+    GlowESP = v
+end})
+=======
 ESP:AddToggle({
     text = "Box Esp",
     state = false,
@@ -498,3 +627,5 @@ LPlayer:AddList({
 })
 
 -- Additional Functions or Code for other sections...
+
+>>>>>>> a8275421e1a283730be080668751bce991702251
