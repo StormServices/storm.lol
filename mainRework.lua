da809ff67184179e1f52115337ac76336243561c
StormServices <officialstormservices@gmail.com>
2024-07-30 17:49:31 -0700

    a

--- mainRework.lua
+++ mainRework.lua
@@ -18,7 +18,7 @@ local Tabs = {
     Legit = Window:AddTab(" Legit "),
     Rage = Window:AddTab(" Rage "),
     Visual = Window:AddTab(" Visual "),
-    AntiAim = Window:AddTab(" Anti-Aim "),
+    -- AntiAim = Window:AddTab(" Anti-Aim "),
     -- Fun = Window:AddTab(" Fun "),
     Misc = Window:AddTab(" Misc "),
     ['UI Settings'] = Window:AddTab('UI Settings'),
@@ -46,10 +46,10 @@ local TeamChamsTab = Chams:AddTab('Team') --               // Team Chams Right T
 local LocalChamsTab = Chams:AddTab('Local') --             // Local Chams Right Tab Box
 local Glow = Tabs.Visual:AddLeftGroupbox('Glow') --        // Glow Left Group box
 
--- // Anti-Aim
+--[[  // Anti-Aim
 local Desync = Tabs.AntiAim:AddLeftGroupbox("Desync")
 local FakeLag = Tabs.AntiAim:AddRightGroupbox("Fake Lag") -- make fake lag
-
+--]]
 -- // Misc
 local Misc = Tabs.Misc:AddLeftGroupbox("Misc") -- idk
 local MiscPlayer = Tabs.Misc:AddRightGroupbox("Player")
@@ -70,6 +70,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Camera = workspace.CurrentCamera
 -- local RayIgnore = workspace.Ray_Ignore
 local LocalPlayer = Players.LocalPlayer
+local mouse = game.Players.LocalPlayer:GetMouse()
 
 ------------------------------------------------------------------------
 
@@ -91,7 +92,12 @@ local AimSettings = {
     EnableFOV = false,
     FOV = 150
 }
-
+local EspSettings = {
+    EspEnabled = false,
+    tracers = false,
+    nESP = false, -- name esp
+    bESP = false -- box esp
+}
 Aimbot:AddToggle('Aimbot', {Text = 'Aimbot', Default = false, Callback = function(Value)
     AimSettings.Enabled = Value
     if Value then
@@ -100,7 +106,7 @@ Aimbot:AddToggle('Aimbot', {Text = 'Aimbot', Default = false, Callback = functio
         FOVring.Visible = false
     end
 end})
-Aimbot:AddSlider('Smoothing', {Text = 'Smoothing', Min = 0, Max = 10, Default = 1, Rounding = 0, Callback = function(Value)
+Aimbot:AddSlider('Smoothing', {Text = 'Smoothing', Min = 0, Max = 10, Default = 1, Rounding = 0, Compact = false, Callback = function(Value)
     AimSettings.Smoothing = Value
 end})
 
@@ -118,20 +124,7 @@ Aimbot:AddDropdown('WhitelistPlayerDropdown', {SpecialType = 'Player', Text = 'W
     -- tablet.insert(SavedWhitelistTable)
 end})
 
-Aimbot:AddLabel('Aim Keybind'):AddKeyPicker('AimKeybind', {
-    Default = 'MB2',
-    SyncToggleState = false,
-    Mode = 'Toggle', -- Modes: Always, Toggle, Hold
-    Text = 'Auto lockpick safes', -- Text to display in the keybind menu
-    NoUI = false, -- Set to true if you want to hide from the Keybind menu,
-    Callback = function(Value)
-        print('[cb] Keybind clicked!', Value)
-    end,
-
-    ChangedCallback = function(New)
-        print('[cb] Keybind changed!', New)
-    end
-})
+Aimbot:AddLabel('Aim Keybind')
 
 LFov:AddToggle('EnableFOV', {Text = 'Enable FOV', Default = false, Callback = function(Value)
     AimSettings.EnableFOV = Value
@@ -140,11 +133,32 @@ LFov:AddToggle('EnableFOV', {Text = 'Enable FOV', Default = false, Callback = fu
     end
 end})
 
-LFov:AddSlider('FOV', {Text = 'FOV', Min = 50, Max = 300, Default = 150, Callback = function(Value)
+LFov:AddSlider('FOV', {Text = 'FOV', Min = 50, Max = 300, Default = 150, Rounding = 1, Compact = false, Callback = function(Value)
     AimSettings.FOV = Value
     FOVring.Radius = Value
 end})
+RPlayer:AddToggle('EnableBhop', {Text = 'Bhop', Default = false, Callback = function(v)
+    if v == true then
+        if LocalPlayer.Character ~= nil and UserInputService:IsKeyDown(Enum.KeyCode.Space) and LocalPlayer.PlayerGui.GUI.Main.GlobalChat.Visible == false then
+            LocalPlayer.Character.Humanoid.Jump = true
+            local Speed = BhopSpeed or 100
+            local Dir = Camera.CFrame.LookVector * Vector3.new(1,0,1)
+            local Move = Vector3.new()
 
+            Move = UserInputService:IsKeyDown(Enum.KeyCode.W) and Move + Dir or Move
+            Move = UserInputService:IsKeyDown(Enum.KeyCode.S) and Move - Dir or Move
+            Move = UserInputService:IsKeyDown(Enum.KeyCode.D) and Move + Vector3.new(-Dir.Z,0,Dir.X) or Move
+            Move = UserInputService:IsKeyDown(Enum.KeyCode.A) and Move + Vector3.new(Dir.Z,0,-Dir.X) or Move
+            if Move.Unit.X == Move.Unit.X then
+                Move = Move.Unit
+                LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(Move.X * Speed, LocalPlayer.Character.HumanoidRootPart.Velocity.Y, Move.Z * Speed)
+            end
+        end
+    end
+end})
+RPlayer:AddSlider('BhopSpeed', {Text = 'Bhop Speed', Default = 15, Min = 0, Max = 100, Rounding = 1, Compact = false, Callback = function(Value)
+    BhopSpeed = v
+end})
 RPlayer:AddToggle('SpinBot', {Text = 'Spinbot', Default = false, callback = function(v)
     SpinbotEnabled = v
     while SpinbotEnabled == true do
@@ -154,14 +168,105 @@ RPlayer:AddToggle('SpinBot', {Text = 'Spinbot', Default = false, callback = func
         else
             game.Players.LocalPlayer.Character.Humanoid.AutoRotate = true
         end
-        task.wait()
+        task.wait() 
     end
 end})
-RPlayer:AddSlider('SpinbotSpeedd', {Text = 'Spinbot Speed', Default = 30, Min = 0, Max = 100, Rounding = 1, Compact = false, Callback = function(Value)
+RPlayer:AddSlider('SpinbotSpeed', {Text = 'Spinbot Speed', Default = 30, Min = 0, Max = 100, Rounding = 1, Compact = false, Callback = function(Value)
     SpinbotSpeed = Value
 end})
 
+EnemiesEspTab:AddLabel('Esp') TeamEspTab:AddLabel('Esp') LocalEspTab:AddLabel('Esp') EnemiesChamsTab:AddLabel('Chams') TeamChamsTab:AddLabel('Chams') LocalChamsTab:AddLabel('Chams')
+local c = workspace.CurrentCamera
+local ps = game:GetService("Players")
+local lp = ps.LocalPlayer
+local rs = game:GetService("RunService")
 
+local function esp(p,cr)
+    local h = cr:WaitForChild("Humanoid")
+    local hrp = cr:WaitForChild("HumanoidRootPart")
+
+    local text = Drawing.new("Text")
+    text.Visible = false
+    text.Center = true
+    text.Outline = true 
+    text.Font = 2
+    text.Color = Color3.fromRGB(255,255,255)
+    text.Size = 13
+
+    local c1
+    local c2
+    local c3
+
+    local function dc()
+        text.Visible = false
+        text:Remove()
+        if c1 then
+            c1:Disconnect()
+            c1 = nil 
+        end
+        if c2 then
+            c2:Disconnect()
+            c2 = nil 
+        end
+        if c3 then
+            c3:Disconnect()
+            c3 = nil 
+        end
+    end
+
+    c2 = cr.AncestryChanged:Connect(function(_,parent)
+        if not parent then
+            dc()
+        end
+    end)
+
+    c3 = h.HealthChanged:Connect(function(v)
+        if (v<=0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
+            dc()
+        end
+    end)
+
+    c1 = rs.RenderStepped:Connect(function()
+        local hrp_pos,hrp_onscreen = c:WorldToViewportPoint(hrp.Position)
+        if hrp_onscreen then
+            text.Position = Vector2.new(hrp_pos.X, hrp_pos.Y)
+            text.Text = p.Name
+            text.Visible = true
+        else
+            text.Visible = false
+        end
+    end)
+end
+
+local function p_added(p)
+    if p.Character then
+        esp(p,p.Character)
+    end
+    p.CharacterAdded:Connect(function(cr)
+        esp(p,cr)
+    end)
+end
+
+EnemiesEspTab:AddToggle('EnableEsp', {Text = 'Enable Esp', Default = false, callback = function(v)
+    EspSettings.EspEnabled = v
+end})
+
+EnemiesEspTab:AddToggle('LineEsp', {Text = 'Tracer / Lines', Default = false, callback = function(v)
+    EspSettings.tracers = v
+end})
+EnemiesEspTab:AddToggle('NameEsp', {Text = 'Name', Default = false, callback = function(v)
+    if EspSettings.EspEnabled == true then
+        for i,p in next, ps:GetPlayers() do 
+            if p ~= lp then
+                p_added(p)
+            end
+        end
+        ps.PlayerAdded:Connect(p_added)
+    end
+end})
+EnemiesEspTab:AddToggle('BoxEsp', {Text = 'Box', Default = false, callback = function(v)
+    EspSettings.tracers = v   
+end})
 Library:SetWatermarkVisibility(true)
 
 -- Notif
@@ -177,7 +282,7 @@ Library:OnUnload(function()
 end)
 
 MenuGroup:AddButton('Unload', function() Library:Unload() end)
-MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'Insert', NoUI = true, Text = 'Menu keybind' })
 
 Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
 MenuGroup:AddToggle('Keybinds', {Text = 'Keybinds', Default = true, Callback = function(Value)
